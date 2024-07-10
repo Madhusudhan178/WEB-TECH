2e8c9ea26db3811d791f38aa5c26a04ec2d6e9b4
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-07-10 19:44:32 +0530

    inital commit

---
+++ javascript/Day-2/script.js
@@ -0,0 +1,34 @@
+// var x=150;
+// console.log(x);
+// if(x%2==0)
+// {
+//      console.log(x," is even number")
+
+// }
+// else
+// {
+//      console.log(x,"  is odd number");
+// }
+let y =Number(prompt("Enter  first number bru"))
+console.log(y);
+let z=Number(prompt("Enter second number bru"))// type conversion
+// console.log(z);
+// if(y>z)
+// {
+//      console.log(y+" is greater than "+z)
+// }
+// else
+// {
+//      console.log(z,"is greater than",y)
+// }
+// console.log(typeof(y));
+// console.log(typeof(z));
+// let sum=0;
+// sum=Number(y)+Number(z)
+// console.log(typeof(sum));
+// let sum1=Number(sum)
+// console.log(typeof(y));
+// console.log(typeof(z));
+// console.log(typeof(sum1));
+// console.log(sum1);
+
