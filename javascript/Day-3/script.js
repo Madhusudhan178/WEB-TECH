ee90e638799c6a270e36bfa09cd4b5c211f3eda7
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-07-11 11:36:53 +0530

    inital commit

---
+++ javascript/Day-3/script.js
@@ -0,0 +1,5 @@
+var nam=prompt("Enter your name:")
+var place=promt("enter your place")
+console.log("Your name is"+nam+"and belongs to"+place)
+console.log(`Your name is ${nam} and belongs to ${place}`)//This works in back tics
+// Error console.log("Your name is ${nam} and belongs to ${place}")
