64abfd9705d0ac25f38ccad9f44bc183a6970915
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-07-12 10:52:48 +0530

    inital commit

---
+++ javascript/Day-3/Divisibleby.js
@@ -0,0 +1,16 @@
+let n=Number(prompt("enter a number:"));
+if(n%3==0 && n%4==0)
+{
+     console.log("SOMESHWAR Private limited");
+}
+else if(n%3==0)
+{
+     console.log("SOMESHWAR");
+}
+else if(n%4==0)
+{
+     console.log("SOMESHWAR Private");
+}
+else{
+     console.log("muchkond javascript odhko")
+}
---
+++ javascript/Day-3/color.js
@@ -0,0 +1,19 @@
+var a=(prompt("Enter a color:"))
+let c=a.toLowerCase();
+console.log(c)
+
+switch(c)
+{
+     case "red":
+          console.log(`${c} is present`);
+          break;
+     case "pink":
+          console.log(`${c} is present`);
+          break;
+     case "green":
+          console.log(`${c} is present`);
+          break;
+     default:
+          console.log(`${c} is  not present`);
+     
+}
