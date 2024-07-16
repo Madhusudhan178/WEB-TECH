af228e2ada0759f8d88737cc635ab3f60922f142
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-07-16 12:32:47 +0530

    inital commit

--- javascript/Day-5/greet.js
+++ javascript/Day-5/greet.js
@@ -1,7 +1,11 @@
 let Name=prompt("Enter your name:")
 
 let greet=function(name){
-     console.log(`Hi ,good morning${name}`);
-     console.log( `Vachi MG ${name}`);
+     console.log(`Hi ,good morning,${name}`);
+     // console.log( `Vachi MG ${name}`);
 }
-greet(Name);
+ greet(Name);
+ console.log(greet);
+ let x=(a,b)=>console.log(a+b)
+
+ x(10,20)
