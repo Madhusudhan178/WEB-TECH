b304bbc0b3737a4886a1c4af5653cc8847a8e516
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-07-31 11:51:02 +0530

    inital commit

--- javascript/Day-10/onclick.js
+++ javascript/Day-10/onclick.js
@@ -1,6 +1,35 @@
-function greet()
-{
-    let y=prompt("Enter your name");
-    document.querySelector("p").innerHTML= `hello ${y} ,welcome`;
-    
+// function greet()
+// {
+//     let y=prompt("Enter your name");
+//     document.querySelector("p").innerHTML=`hello ${y} ,welcome` ;
+
+// }
+
+
+
+// let z=document.querySelector("button")
+// console.log(z)//<button>click here</button>
+// z.onclick=function f1()
+// {
+//     let y=prompt("Enter your name");
+//     document.querySelector("h3").innerHTML=`hello ${y} ,welcome`;
+// }
+
+
+
+// addEventListener(event,function)
+//let y=document.querySelector("button")
+// addEventListener("click",function greet()
+// {
+//     let z=prompt("Enter your name");
+//     document.querySelector("h3").innerHTML=`hello ${z} ,welcome`;
+// })
+
+function print(){
+let i=document.querySelector("input");
+console.log(i);
+let v=i.value;
+console.log(v);
+let ans=document.querySelector("p")
+ans.innerHTML=v;
 }
