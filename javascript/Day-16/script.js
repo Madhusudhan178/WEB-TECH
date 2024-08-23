f95251e2c268604cc2545244ce4728026da93724
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-08-23 17:18:58 +0530

    inital commit

--- javascript/Day-16/script.js
+++ javascript/Day-16/script.js
@@ -1,2 +1,38 @@
-console.log("Hello")
-// console.dir(window.document)
+// console.log("Hello")
+// // console.dir(window.document)
+// const a=1;
+
+// console.log(a);
+// let name ="madhu";
+// let age=18;
+// let isMarried= false ;
+// console.log("this person name is "+name+" and their age is"+age)
+
+// let arr=[10,20,30,40,1,2,5,500,250,900]
+// let largest=arr[0];
+// console.log(largest);
+// for(let i=0;i<arr.length;i++)
+// {
+//     largest =largest>arr[i]?largest:arr[i];
+//     //console.log(largest);
+
+// }
+// console.log(largest);
+// console.log(1-(-1))
+
+
+
+
+// var b=100;
+// var z=10000;
+// {
+//     var z=10;
+//     let b=20;
+//     const c=30;
+    
+//     console.log(b)
+//     console.log(z)
+// }
+// console.log(z)
+
+
